8b66f13c67bf39f5c38b3ee446096b3103d4cbc8
Calypso Charm <pidgey@pidgeyassist.com>
2026-04-02 14:05:35 -0600

    Initialize Atomagachi Desktop codebase

--- main.js
+++ main.js
@@ -2,13 +2,24 @@ const { app, BrowserWindow, screen, ipcMain, globalShortcut, Tray, Menu, nativeI
 const http = require('http');
 const { spawn } = require('child_process');
 
-let win;
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+  app.quit();
+  process.exit(0);
+}
+
+app.on('second-instance', () => {
+  if (catWin) catWin.focus();
+});
+
+let catWin;
+let widgetWin;
 let tray = null;
 
 function createWindow () {
   const { width, height } = screen.getPrimaryDisplay().workAreaSize;
   
-  win = new BrowserWindow({
+  catWin = new BrowserWindow({
     width: width,
     height: height, 
     x: 0,
@@ -23,15 +34,33 @@ function createWindow () {
       contextIsolation: false
     }
   });
+  catWin.setIgnoreMouseEvents(true, { forward: true });
+  catWin.loadFile('index.html', { query: { mode: 'cat' } });
 
-  win.setIgnoreMouseEvents(true, { forward: true });
+  widgetWin = new BrowserWindow({
+    width: width,
+    height: height, 
+    x: 0,
+    y: 0,
+    transparent: true,
+    frame: false,
+    alwaysOnTop: false,
+    skipTaskbar: true,
+    hasShadow: false,
+    focusable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  widgetWin.setIgnoreMouseEvents(true, { forward: true });
+  widgetWin.loadFile('index.html', { query: { mode: 'widgets' } });
 
-  win.loadFile('index.html');
 
   // Register a global shortcut
   globalShortcut.register('CommandOrControl+N', () => {
-    if (win) {
-      win.webContents.send('show-notification', "You have a new message from a friend! 💌");
+    if (catWin) {
+      catWin.webContents.send('show-notification', "You have a new message from a friend! 💌");
     }
   });
 
@@ -40,9 +69,9 @@ function createWindow () {
   try {
      const server = http.createServer((req, res) => {
        if (req.url.includes('/watchdog')) {
-         if (win) {
-             win.webContents.send('watchdog-alert');
-             win.webContents.send('show-notification', '[CRITICAL EVENT]\nWATCHDOG FLAG TRIPPED!');
+         if (catWin) {
+             catWin.webContents.send('watchdog-alert');
+             catWin.webContents.send('show-notification', '[CRITICAL EVENT]\nWATCHDOG FLAG TRIPPED!');
          }
          res.end('Watchdog alerted to pet.\n');
        } else {
@@ -76,7 +105,7 @@ while ($true) {
          const appStr = line.trim();
          if (appStr && appStr !== currentApp) {
              currentApp = appStr;
-             if (win) win.webContents.send('app-changed', currentApp);
+             if (catWin) catWin.webContents.send('app-changed', currentApp);
          }
       }
   });
@@ -85,12 +114,12 @@ while ($true) {
   let lastRx = 0, lastTx = 0;
   setInterval(() => {
     require('child_process').exec('netstat -e', (err, stdout) => {
-        if (!err && win) {
+        if (!err && (catWin || widgetWin)) {
             const match = stdout.match(/Bytes\s+(\d+)\s+(\d+)/);
             if (match) {
                 let rx = parseInt(match[1]); let tx = parseInt(match[2]);
                 if (lastRx !== 0) {
-                    win.webContents.send('net-stats', { rx: (rx - lastRx) / 2, tx: (tx - lastTx) / 2 });
+                    if (widgetWin) widgetWin.webContents.send('net-stats', { rx: (rx - lastRx) / 2, tx: (tx - lastTx) / 2 });
                 }
                 lastRx = rx; lastTx = tx;
             }
@@ -108,19 +137,44 @@ ipcMain.on('set-ignore-mouse-events', (event, ignore, options) => {
   if (targetWin) targetWin.setIgnoreMouseEvents(ignore, options);
 });
 
+ipcMain.on('broadcast-toggle-layout', () => {
+  if (widgetWin) widgetWin.webContents.send('toggle-layout');
+});
+
+ipcMain.on('broadcast-theme-update', () => {
+  if (widgetWin) widgetWin.webContents.send('theme-update');
+});
+
 ipcMain.on('console', (event, msg) => {
   console.log('[RENDERER] ' + msg);
 });
 
+ipcMain.on('set-always-on-top', (event, isAlwaysOnTop) => {
+  if (catWin) {
+    catWin.setAlwaysOnTop(isAlwaysOnTop);
+  }
+});
+
 app.whenReady().then(() => {
   createWindow();
+  
+  screen.on('display-metrics-changed', () => {
+     const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+     if (catWin) { catWin.setBounds({ x: 0, y: 0, width, height }); }
+     if (widgetWin) { widgetWin.setBounds({ x: 0, y: 0, width, height }); }
+  });
   
   const path = require('path');
   const iconPath = path.join(__dirname, 'icon.png');
   tray = new Tray(iconPath);
   tray.setToolTip('Atomagachi AI Core');
   tray.setContextMenu(Menu.buildFromTemplate([
-    { label: 'Reset Component Layout', click: () => { if (win) win.webContents.executeJavaScript('window.resetLayout()'); } },
+    { label: 'Restart Core', click: () => { 
+        if (process.env.PORTABLE_EXECUTABLE_FILE) { app.relaunch({ execPath: process.env.PORTABLE_EXECUTABLE_FILE }); } 
+        else { app.relaunch(); } 
+        app.quit(); 
+    } },
+    { label: 'Reset Component Layout', click: () => { if (catWin) catWin.webContents.executeJavaScript('window.resetLayout()'); if (widgetWin) widgetWin.webContents.executeJavaScript('window.resetLayout()'); } },
     { type: 'separator' },
     { label: 'Shutdown OS', click: () => { app.quit(); } }
   ]));
